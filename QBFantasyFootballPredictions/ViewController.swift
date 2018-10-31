13a1013e480cb0600c31a15a8398db4ee8ad430d
flizzer <briandavis1977@gmail.com>
2018-10-30 22:16:41 -0400

    Initial Commit

---
+++ QBFantasyFootballPredictions/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QBFantasyFootballPredictions
+//
+//  Created by Brian Davis on 10/30/18.
+//  Copyright © 2018 Brian Davis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
